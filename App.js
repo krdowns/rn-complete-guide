d05310a7dc236b53ca3cbe7db4ccfc4494630192
Kenny Downs <kdowns@repairpal.com>
2020-02-20 15:51:39 -0800

    now has add functionality and displays added goals

--- App.js
+++ App.js
@@ -3,9 +3,14 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
   };
 
   return (
@@ -17,9 +22,18 @@ export default function App() {
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <View>
+        {courseGoals.map(goal => (
+          <View>
+            <Text>
+              {" "}
+              key={goal}>{goal}
+            </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
