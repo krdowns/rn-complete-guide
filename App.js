f76c0eb97bdb99a3e5d12692254cb8a3b6c8e17b
Kenny Downs <kdowns@repairpal.com>
2020-02-23 11:44:48 -0800

    fixed key error message with key extractor

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ export default function App() {
     <Button title="Add New Goal" onPress={() => setIsAddMode(true)}/>
       <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler} />
       <FlatList
-        keyExtractor={(item, index) => item.key}
+        keyExtractor={(item, index) => item.id}
         data={courseGoals}
         renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value}/>}
       />
